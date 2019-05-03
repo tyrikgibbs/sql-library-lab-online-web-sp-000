5214358f3ee42ce47a4178c23a19c90b21a479e3
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-05-03 21:05:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN characters WHERE species = 'human' ORDER BY (series.title) DESC LIMIT 1"
+  "SELECT series.title FROM series INNER JOIN COUNT(characters.species) WHERE species = 'human' ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
